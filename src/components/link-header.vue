347d5f6a0ac26d454bc896742f382b7b98ca6132
lovesky <aidarkhanov.a@chocomart.kz>
2018-10-08 10:33:00 +0600

    test commit.

--- src/components/link-header.vue
+++ src/components/link-header.vue
@@ -4,7 +4,7 @@
       <div class="row linkHeader">
         <div class="linkHeader-support">
           <!--<chocomart-icon :color="'#fff'" :font_size="'20px'" :icon="'bonus'"></chocomart-icon>-->
-          <!--<chocomart-icon :color="'purple'" :font_size="'50px'" :icon="'heart'"></chocomart-icon>-->
+          <chocomart-icon :color="'purple'" :font_size="'50px'" :icon="'heart'"></chocomart-icon>
 
         </div>
         <div class="linkHeader-phones">
