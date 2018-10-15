8707b95501227a433d9be44cd990eeee83d8b58d
lovesky <aidarkhanov.a@chocomart.kz>
2018-10-15 12:26:51 +0600

    fixed some width

--- src/components/link-header.vue
+++ src/components/link-header.vue
@@ -4,7 +4,7 @@
       <div class="content-wrapper__feedback feedback-wrapper h-100 d-flex align-items-center justify-content-center">
         Поддержка
         <div class="feedback-wrapper__arrow-down"></div>
-        <div class="feedback-wrapper__dropdown dropdown-wrapper w-100 flex-column">
+        <div class="feedback-wrapper__dropdown dropdown-wrapper flex-column">
           <div class="dropdown-wrapper__type type-wrapper w-100 d-flex align-items-center">
             <div class="type-wrapper__icon h-100 d-flex align-items-center justify-content-center">
               <chocomart-icon :icon="'credit-card'" :font_size="'26px'"></chocomart-icon>
